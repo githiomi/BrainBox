976c468d3b5e25be703d9cfe3984de666a11bd2d
githiomi <danielgithiomi@gmail.com>
2024-01-02 16:20:28 +0400

    Ref: Removed unneccessary classes and configs

--- BrainBox Backend/brainbox/src/main/java/com/dhosiolux/brainbox/AppConfig.java
+++ BrainBox Backend/brainbox/src/main/java/com/dhosiolux/brainbox/AppConfig.java
@@ -1,11 +1,12 @@
 package com.dhosiolux.brainbox;
 
+import com.dhosiolux.brainbox.models.ColorCodes;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
+import java.time.LocalDateTime;
 
 @Configuration
 public class AppConfig {
@@ -13,8 +14,7 @@ public class AppConfig {
     @Bean
     CommandLineRunner commandLineRunner(ApplicationContext applicationContext){
         return args -> {
-            String[] beans = applicationContext.getBeanDefinitionNames();
-            Arrays.sort(beans);
+            System.out.println(ColorCodes.BLUE + "\nServer started successfully at: " + LocalDateTime.now() + "/n" + ColorCodes.RESET);
         };
     }
 }
