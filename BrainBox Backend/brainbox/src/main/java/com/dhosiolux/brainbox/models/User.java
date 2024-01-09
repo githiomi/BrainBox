2ca91dad05c1df4f071720922ae6179f84d14005
githiomi <danielgithiomi@gmail.com>
2024-01-09 21:32:49 +0400

    Feat: Created user class model to create objects

--- BrainBox Backend/brainbox/src/main/java/com/dhosiolux/brainbox/models/User.java
+++ BrainBox Backend/brainbox/src/main/java/com/dhosiolux/brainbox/models/User.java
@@ -1,12 +1,29 @@
 package com.dhosiolux.brainbox.models;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Random;
 import java.util.UUID;
 
+@Data
 public class User {
     final UUID userId = UUID.randomUUID();
     String firstName;
     String lastName;
     String username;
     Role userRole;
+
+    public User(String firstName, String lastName, Role role){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = (firstName.charAt(0) + lastName.substring(0,3)).toUpperCase() + generateNumberSuffix();
+    }
+
+    private String generateNumberSuffix(){
+        int min = 100, max = 999;
+        int randomInt = new Random().nextInt((max - min) + 1) + min;
+        return String.valueOf(randomInt);
+    }
 
 }
