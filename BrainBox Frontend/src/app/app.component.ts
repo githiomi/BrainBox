2aaec89500bdd7283809972038fdacdedadc5f34
githiomi <danielgithiomi@gmail.com>
2024-01-26 12:39:46 +0400

    Chore: Code clean of unncessary code

--- BrainBox Frontend/src/app/app.component.ts
+++ BrainBox Frontend/src/app/app.component.ts
@@ -1,10 +1,6 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { Observable, map, tap } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import { Response } from '../models/response';
-import { Event } from '../models/event';
 
 @Component({
   selector: 'app-root',
@@ -13,21 +9,7 @@ import { Event } from '../models/event';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'brainbox';
-  events$ !: Observable<Event[]>;
-  private readonly baseUrl = 'http://localhost:8080/api/v1.0/events';
+export class AppComponent {
+  
 
-  private _httpClient : HttpClient = inject(HttpClient);
-
-  ngOnInit(){
-    this.events$ = this.getAllEvents();
-  }
-
-  getAllEvents() : Observable<Event[]> {
-    return this._httpClient.get<Event[]>(this.baseUrl).pipe(
-      tap(console.log),
-      map( (_res : Response<Event[]>) => _res.resource)
-    )
-  }
 }
