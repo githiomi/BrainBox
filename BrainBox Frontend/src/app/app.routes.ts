1bc05fd537fba506111f76e337f0a71e8a3691a9
githiomi <danielgithiomi@gmail.com>
2024-01-26 11:52:10 +0400

    Feat: Setup for basic application routes complete

--- BrainBox Frontend/src/app/app.routes.ts
+++ BrainBox Frontend/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent,
+        title: 'BrainBox Home'
+    },
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    }
+];
