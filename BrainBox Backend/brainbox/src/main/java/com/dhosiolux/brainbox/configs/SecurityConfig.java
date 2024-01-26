9af6e3a40bb29e39c809e9a6feec96a4c570e0e8
githiomi <danielgithiomi@gmail.com>
2024-01-26 11:23:25 +0400

    Chore: Last backend commit

--- BrainBox Backend/brainbox/src/main/java/com/dhosiolux/brainbox/configs/SecurityConfig.java
+++ BrainBox Backend/brainbox/src/main/java/com/dhosiolux/brainbox/configs/SecurityConfig.java
@@ -3,5 +3,5 @@ package com.dhosiolux.brainbox.configs;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class SecurityConfig {
+public class  SecurityConfig {
 }
