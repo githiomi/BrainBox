047067c6a5520e62d21438bb69c8972a0a7c8014
githiomi <danielgithiomi@gmail.com>
2024-01-28 14:45:07 +0400

    Feat: Header basic structure and styling complete

--- BrainBox Frontend/src/app/components/utilities/header/header.component.ts
+++ BrainBox Frontend/src/app/components/utilities/header/header.component.ts
@@ -1,13 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  readonly logoUrl : string = './../../../../assets/images/logo.png';
 
 }
