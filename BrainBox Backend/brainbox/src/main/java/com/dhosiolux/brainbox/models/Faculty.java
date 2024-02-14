079ff35411147e46d487a94a127d6142f1492b20
githiomi <danielgithiomi@gmail.com>
2024-02-14 10:22:31 +0400

    Feat: Confirmed DB connection with dummy class

--- BrainBox Backend/brainbox/src/main/java/com/dhosiolux/brainbox/models/Faculty.java
+++ BrainBox Backend/brainbox/src/main/java/com/dhosiolux/brainbox/models/Faculty.java
@@ -1,4 +1,21 @@
 package com.dhosiolux.brainbox.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "faculty_table")
 public class Faculty {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int facultyId;
+    private String facultyName;
 }
